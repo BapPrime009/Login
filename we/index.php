61779bf690a6a0cd169faca2ebf6f966d3cb3e8c
BapPrime009 <93791187+BapPrime009@users.noreply.github.com>
2021-11-15 15:36:42 -0500

    login para inicio de secion (php, css)
    se hace un login capaz de buscar usuario y contraseña en una base de datos y loguearte en un dashboard simple

---
+++ we/index.php
@@ -0,0 +1,80 @@
+<?php
+    $alert = "";
+    session_start();
+    if(!empty($_SESSION['active']))
+        {
+            header('location: system/');
+        }else{
+
+    if(!empty($_POST)){
+        if(empty($_POST['email']) || empty($_POST['password'])){
+            $alert = '<p class="alert alert-danger"> Ingrese su Usuario y Contraseña </p>';
+        }else{
+            require_once "conexion.php";
+
+            $user = mysqli_real_escape_string($conection,$_POST['email']);
+            $pass = mysqli_real_escape_string($conection,$_POST['password']);
+
+            $query = mysqli_query($conection,"SELECT * FROM users WHERE email = '$user' AND password = '$pass'");
+            $result = mysqli_num_rows($query);
+
+            if($result > 0){
+                $data = mysqli_fetch_array($query);
+                
+                $_SESSION['active'] = true;
+                $_SESSION['correo'] = $data['correo'];
+                
+
+                header('location: system/');
+            }else{
+                $alert = '<p class="alert alert-warning">Usuario o Contraseña incorrecta</p>';
+                session_destroy();
+            }
+        }
+
+    }
+}
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+<title>Login</title>
+    </head>
+<body>
+    <form method="post">
+    <div class="Container">
+
+        <div class="loginx">
+            <h1>Login</h1>
+            <input type="email" placeholder="Correo@xample.com" name="email"><br><br>
+            <input type="password" placeholder="contrasena" name="password"><br><br>
+            <input type="submit" name="Enviar">  
+        </div>
+    </div>
+</form>
+<style>
+    .Container{
+        background: rgb(0, 60, 255);
+  height: 500px;
+  width: 500px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+    }
+    .loginx{
+        background: rgb(210, 205, 224);
+        width: 200px;
+        padding: 3px;
+        border-radius: 1px;
+        
+    }
+    *{
+    margin: 0;
+    padding: 0;
+    font-family: Arial;
+    color: rgb(76, 0, 255);
+}
+
+</style>
+</body>
+    </html>
---
+++ we/system/index.php
@@ -0,0 +1,9 @@
+<?php
+echo 'lalala'
+?>
+    <h1>hola</h1>
+    <p>si lees esto me debes una empanada :)</p>
+<div>
+    <a href="salir.php">salir</a>
+</div>
+
